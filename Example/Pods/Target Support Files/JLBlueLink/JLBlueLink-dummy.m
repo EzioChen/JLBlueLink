2db450c8c5817732a1fe4de6d12d79b80379cf74
eziochan <jackenwind@163.com>
2024-12-25 09:03:21 +0800

    init

---
+++ Example/Pods/Target Support Files/JLBlueLink/JLBlueLink-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JLBlueLink : NSObject
+@end
+@implementation PodsDummy_JLBlueLink
+@end
---
+++ Example/Pods/Target Support Files/Pods-JLBlueLink_Tests/Pods-JLBlueLink_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JLBlueLink_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JLBlueLink_Tests
+@end
